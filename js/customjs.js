083c774f1ea6738c45122ab90dbceee4aa29c9fe
kokojek <60446520+kokojek@users.noreply.github.com>
2020-12-02 23:36:37 +0100

    vue
    jeg forsøger mig med noget vue men det viser sig til at være udfordrende :C

--- js/customjs.js
+++
@@ -1,3 +0,0 @@
-// JavaScript Document
-/*JS for overview of mead, beer and other items*/
-
---
+++ js/vue.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+/*Vue for overview of mead, beer and other items*/
+
+/*Vue for accordion*/
